a4c294d6e21451e65a82117cdc45e7939d9b1ba5
cantfindid24 <106964984+cantfindid24@users.noreply.github.com>
2024-01-22 20:15:02 +0530

    cookies:learning about reading/accessing and sending cookies from server

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 import express from 'express';
 import connectMongoDb from './connection.js';
+import cookieParser from 'cookie-parser';
 import auth from './route/auth.js';
 const PORT = 8001;
 const app = express();
@@ -10,6 +11,9 @@ connectMongoDb('mongodb://127.0.0.1/node-express-jwt-auth').then(() =>
 
 // middleware
 app.use(express.static('public'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
 
 // view engine
 app.set('view engine', 'ejs');
@@ -17,13 +21,29 @@ import path from 'path';
 const __dirname = path.resolve();
 app.set('views', path.join(__dirname, '/views'));
 
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-
 // routes
 app.get('/', (req, res) => res.render('home'));
 app.get('/smoothies', (req, res) => res.render('smoothies'));
 app.use('/', auth);
+
+//cookies learning
+app.get('/set-cookies', (req, res) => {
+  res.setHeader('Set-Cookie', 'cookieKey1=value1');
+  res.setHeader('Set-Cookie', 'cookieKey2=value2');
+  res.setHeader('Set-Cookie', 'newUser=false');
+  res.setHeader('Set-Cookie', 'newUser=true');
+  res.cookie('key', 'value', 'ohter');
+  res.cookie('newUser', false);
+  res.cookie('isEmployee', true);
+  res.cookie('hello', true, { secure: true });
+  res.cookie('isBoss', true, { maxAge: 1000 * 10, httpOnly: true }); //10sec
+
+  res.send('You got the cookie');
+});
+app.get('/read-cookies', (req, res) => {
+  const cookies = req.cookies;
+  res.json(cookies);
+});
 
 app.listen(PORT, () => {
   console.log(`Server is listening on port: ${PORT}`);
